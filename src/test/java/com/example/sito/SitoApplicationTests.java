e43ae8b847feecfe1a9c54c2ec13ced6f36a505f
Orion98-svg <shahnewazkazi7@gmail.com>
2021-02-24 20:20:34 +0100

    New Project primo commit

---
+++ src/test/java/com/example/sito/SitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
